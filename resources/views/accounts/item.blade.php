59c22a3548f3ed5d1fa07f0f731bba533056fcf9
DESKTOP-11KO03I\student <231075@st.yoshida-g.ac.jp>
2024-06-11 14:28:27 +0900

    見た目の改良

--- resources/views/accounts/item.blade.php
+++ resources/views/accounts/item.blade.php
@@ -1,7 +1,16 @@
-<html lang="ja">
+<html lang="ja" data-bs-theme="dark">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="/app.css">
+</head>
 <body>
 <h1>■アイテム一覧</h1>
 <table>
+    <thead>
     <tr>
         <th>ID</th>
         <th>名前</th>
@@ -9,6 +18,7 @@
         <th>効果値</th>
         <th>説明</th>
     </tr>
+    </thead>
 
     @foreach($accounts as $account)
         <tr>
@@ -24,5 +34,8 @@
     @csrf
     <input type="submit" value="戻る">
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
 </body>
 </html>
